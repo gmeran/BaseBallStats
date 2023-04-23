03a8b1f6b6d6e580ee4f8719e59d3588f5b649e5
gmeran <36177020+gmeran@users.noreply.github.com>
2023-04-23 11:25:50 -0400

    Add project files.

---
+++ BaseBall Stats/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace BaseBall_Stats
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
