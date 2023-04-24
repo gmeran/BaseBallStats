32352876c2aa3c1c7f3dbaa8c83a15d24f47aa63
gmeran <36177020+gmeran@users.noreply.github.com>
2023-04-24 13:57:59 -0400

    Added Home Screen that lists Players

--- BaseBall Stats/Controllers/HomeController.cs
+++ BaseBall Stats/Controllers/HomeController.cs
@@ -3,14 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BaseBall_Stats.Models;
+using System.Data.SqlClient;
 
 namespace BaseBall_Stats.Controllers
 {
+    
     public class HomeController : Controller
     {
+        private Gabe_DB db = new Gabe_DB();
         public ActionResult Index()
         {
-            return View();
+            List<Player> players = GetPlayers();
+            return View(players);
+        }
+
+        private List<Player> GetPlayers()
+        {
+            var connection = db.Database.Connection;
+            var exist = db.Database.Exists();
+            if(exist && connection !=null)
+            {
+                try
+                {
+                    List<Player> players = db.Players.ToList();
+                    return players;
+                }
+                catch(Exception ex)
+                {
+                    throw new Exception(ex.ToString());
+                }
+            }
+            else
+            {
+                throw new Exception("Something went wrong when trying to connect to Database");
+            }
         }
 
         public ActionResult About()
---
+++ BaseBall Stats/Models/PlayerViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BaseBall_Stats.Models
+{
+    public class PlayerViewModel
+    {
+        public int PlayerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Position { get; set; }
+        public int YearStated { get; set; }
+        public int YearEnded { get; set; }
+    }
+}
